05b3e786ca6d81f5edd6a758928c2fffcc900a5a
vajiralasantha <vajee555@gmail.com>
2019-04-03 16:06:39 +1300

    Demo update and code refactoring.

--- src/Holiday.php
+++ src/Holiday.php
@@ -19,36 +19,114 @@ class Holiday {
     const SATURDAY = 6;
     const SUNDAY = 7;
 
-    function __construct(  $title, $nominal_day, $nominal_month, $mondayisation_mode, $mondayisation_effective_year, $directive, $easter_relative) {
+    /**
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @var int
+     */
+    public $nominalDay;
+
+    /**
+     * @var int
+     */
+    public $nominalMonth;
+
+    /**
+     * @var int
+     */
+    public $mondayisationMode;
+
+    /**
+     * @var null|int
+     */
+    public $mondayisationEffectiveYear;
+
+    /**
+     * @var null|string
+     */
+    public $directive;
+
+    /**
+     * @var bool
+     */
+    public $easterRelative;
+
+    /**
+     * Holiday default constructor.
+     *
+     * @param $title
+     * @param $nominalDay
+     * @param $nominalMonth
+     * @param $mondayisationMode
+     * @param $mondayisationEffectiveYear
+     * @param $directive
+     * @param $easterRelative
+     */
+    public function __construct($title, $nominalDay, $nominalMonth, $mondayisationMode, $mondayisationEffectiveYear, $directive, $easterRelative) {
         $this->title = $title;
-        $this->nominal_day = $nominal_day;
-        $this->nominal_month = $nominal_month;
-        $this->mondayisation_mode = $mondayisation_mode;
-        $this->mondayisation_effective_year = $mondayisation_effective_year;
+        $this->nominalDay = $nominalDay;
+        $this->nominalMonth = $nominalMonth;
+        $this->mondayisationMode = $mondayisationMode;
+        $this->mondayisationEffectiveYear = $mondayisationEffectiveYear;
         $this->directive = $directive;
-        $this->easter_relative = $easter_relative;
+        $this->easterRelative = $easterRelative;
     }
 
-    static function weekend() {
+    /**
+     * Function to get weekend day number of the week.
+     *
+     * @return array
+     */
+    public static function getWeekend() {
         return array(self::SATURDAY, self::SUNDAY);
     }
 
-    static function isWeekend($dt) {
-        return in_array(date('N', $dt), self::weekend());
+    /**
+     * Function to see if a given date is weekend.
+     *
+     * @param int $date Unix timestamp of the date
+     *
+     * @return bool
+     */
+    public static function isWeekend($date) {
+        return in_array(date('N', $date), self::getWeekend());
     }
 
-    static function yearStartDate($y) {
-        return strtotime("first day of january $y");
+    /**
+     * Function to determine the 1st day of a given year.
+     *
+     * @param int $year Year
+     *
+     * @return false|int
+     */
+    public static function yearStartDate($year) {
+        return strtotime("first day of january $year");
     }
 
-    static function DoW($dt) {
-        return date('N',$dt);
+    /**
+     * Function to get the day number of the week.
+     *
+     * @param int $date Unix timestamp of the date
+     *
+     * @return false|string
+     */
+    public static function dayOfTheWeek($date) {
+        return date('N', $date);
     }
 
-    static function closest($dt, $dta) {
+    /**
+     * @param int $date
+     * @param array $dates
+     *
+     * @return null|int
+     */
+    static function closest($date, $dates) {
         $c = null;
-        foreach($dta as $d) {
-            if ($c == null || abs($dt - $d) < abs($dt - $c)) {
+        foreach($dates as $d) {
+            if ($c == null || abs($date - $d) < abs($date - $c)) {
                 $c = $d;
             }
         }
@@ -56,26 +134,50 @@ class Holiday {
         return $c;
     }
 
-    function nominalDate($y) {
-        return mktime (0, 0, 0, $this->nominal_month, $this->nominal_day, $y);
+    /**
+     * Function to get the nominal date of a given year.
+     *
+     * @param int $year Year
+     *
+     * @return false|int
+     */
+    public function nominalDate($year) {
+        return mktime (0, 0, 0, $this->nominalMonth, $this->nominalDay, $year);
     }
 
-    static function easterSunday($year, $format = 'd.m.Y') {
+    /**
+     * @param int $year year
+     * @param string $format Date format
+     *
+     * @return false|int
+     *
+     * @throws \Exception
+     */
+    public static function easterSunday($year, $format = 'd.m.Y') {
         $easter = new DateTime('@' . easter_date($year));
         $easter->setTimezone(new DateTimeZone('Pacific/Auckland')); //NB: workaround for timezone-related bugs in PHP easter_date
         return strtotime($easter->format($format));
     }
 
-    function occurrence($year) {
+    /**
+     * Function to get holiday occurrence of the year
+     *
+     * @param int $year Year
+     *
+     * @return false|int|null
+     *
+     * @throws \Exception
+     */
+    public function occurrence($year) {
         if ($this->directive) {
-            $observed = strtotime($this->directive, $this->easter_relative ? self::easterSunday($year) : self::yearStartDate($year));
+            $observed = strtotime($this->directive, $this->easterRelative ? self::easterSunday($year) : self::yearStartDate($year));
         } else {
             $observed = $this->nominalDate($year);
         }
 
-        if ($this->mondayisation_mode) {
-            if ($this->mondayisation_effective_year < $year) {
-                switch ($this->mondayisation_mode) {
+        if ($this->mondayisationMode) {
+            if ($this->mondayisationEffectiveYear < $year) {
+                switch ($this->mondayisationMode) {
                     case self::MONDAYISED:
                         if (self::isWeekend($observed)) {
                             $observed = strtotime('next monday', $observed);
--- src/NZHolidays.php
+++ src/NZHolidays.php
@@ -8,7 +8,10 @@ use SplObjectStorage;
 
 class NZHolidays extends SplObjectStorage {
 
-    function __construct() {
+    /**
+     * NZHolidays default constructor.
+     */
+    public function __construct() {
         $this->attach(new Holiday('New Years Day', 1, 1, Holiday::TUESDAYISED, null, null, false));
         $this->attach(new Holiday('Day After New Years Day',2, 1, Holiday::MONDAYISED, null,null,false));
         $this->attach(new Holiday('Waitangi Day', 6, 2, Holiday::MONDAYISED, 2014, null,false));
@@ -25,25 +28,54 @@ class NZHolidays extends SplObjectStorage {
 
     }
 
-    function occurrence($year) {
+    /**
+     * Function to get holiday occurrence of the year
+     *
+     * @param int $year Year
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    private function occurrence($year) {
         $occurrence = array();
         foreach($this as $holiday) {
             /* @var $holiday Holiday */
             $occurrence[] = $holiday->occurrence($year);
         }
+
         return $occurrence;
     }
 
-    function isWorkday($dateInt) {
+    /**
+     * Function to calculate if given date is working date or not.
+     *
+     * @param int $dateInt Unix timestamp of the date
+     *
+     * @return bool|null
+     */
+    public function isWorkday($dateInt) {
         $year = date('Y',$dateInt);
-        $dt = new DateTime('now');
-        $dt->setTimestamp($dateInt);
-        $dt->setTime(0, 0, 0);
+        try {
+            $dt = new DateTime('now');
+            $dt->setTimestamp($dateInt);
+            $dt->setTime(0, 0, 0);
 
-        return ! (Holiday::isWeekend($dt->getTimestamp()) || in_array($dt->getTimestamp(), $this->occurrence($year)));
+            return ! (Holiday::isWeekend($dt->getTimestamp()) || in_array($dt->getTimestamp(), $this->occurrence($year)));
+        } catch (\Exception $e) {
+            return null;
+        }
     }
 
-    function workHoursHence($dateInt, $leadTimeHours) {
+    /**
+     * Function to calculate working hour since a given date.
+     *
+     * @param int $dateInt Current Unix timestamp
+     * @param int $leadTimeHours Number of hours forward
+     *
+     * @return int Calculated Unix timestamp
+     */
+    public function workHoursHence($dateInt, $leadTimeHours) {
         $target = $dateInt;
         while ($leadTimeHours > 0) {
             $target = strtotime('+1 hours', $target);
@@ -55,19 +87,29 @@ class NZHolidays extends SplObjectStorage {
         return $target;
     }
 
-    function workDaysHence($dateInt, $leadTimeDays) {
-        return $this->workHoursHence($dateInt, $leadTimeDays*24);
+    /**
+     * Function to calculate working days since a given date.
+     *
+     * @param int $dateInt Current Unix timestamp
+     * @param int $leadTimeDays Number of days forward
+     *
+     * @return int Calculated Unix timestamp
+     */
+    public function workDaysHence($dateInt, $leadTimeDays) {
+        return $this->workHoursHence($dateInt, $leadTimeDays * 24);
     }
 
     /**
-     * returns number of working hours between given two days
+     * Function to return number of working hours between given two days.
      *
-     * @param DateTime $start
-     * @param DateTime $end
+     * @param DateTime $start Start date
+     * @param DateTime $end End date
      *
-     * @return int
+     * @return int Number of hours
+     *
+     * @throws \Exception
      */
-    function workHoursCount($start, $end) {
+    public function workHoursCount($start, $end) {
         $oneHour = new DateInterval("PT1H");
         $hours = 0;
         $nextHour = new DateTime("@".$end->getTimestamp());
@@ -82,22 +124,24 @@ class NZHolidays extends SplObjectStorage {
     }
 
     /**
-     * returns number of working days between given two days
+     * Function to return number of working days between given two days.
      *
-     * @param DateTime $start
-     * @param DateTime $end
+     * @param DateTime $start Start date
+     * @param DateTime $end End date
      *
-     * @return int
+     * @return int Number of days
+     *
+     * @throws \Exception
      */
-    function workDaysCount($start, $end) {
-        $oneday = new DateInterval("P1D");
+    public function countWorkDays($start, $end) {
+        $oneDay = new DateInterval("P1D");
         $nextDay = new DateTime("@".$end->getTimestamp());
-        $nextDay->add($oneday);
+        $nextDay->add($oneDay);
         $days = 0;
         /* Iterate from $start up to $end+1 day, one day in each iteration.
           We add one day to the $end date, because the DatePeriod only iterates up to,
           not including, the end date. */
-        foreach (new DatePeriod($start, $oneday, $nextDay) as $day) {
+        foreach (new DatePeriod($start, $oneDay, $nextDay) as $day) {
             if ($this->isWorkday($day->getTimestamp())) {
                 $days++;
             }
